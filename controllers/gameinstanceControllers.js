58beb33435cbcd777f40971139dd65e53f07f118
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-03-01 15:14:24 -0800

    Game instance controller skeleton

---
+++ controllers/gameinstanceControllers.js
@@ -0,0 +1,42 @@
+const GameInstance = require("../models/gameinstance");
+const asyncHandler = require("express-async-handler");
+
+// Display list of all GameInstances
+exports.gameinstance_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED:GameInstance List");
+});
+
+// Display detail page for a specific GameInstance
+exports.gameinstance_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
+});
+
+// Display GameInstance create form on GET
+exports.gameinstance_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: GameInstance create GET");
+});
+
+// Handle BookInstance create on POST
+exports.gameinstance_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: GameInstance create POST");
+});
+
+// Display GameInstance delete form on GET
+exports.gameinstance_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: GameInstance delete GET");
+});
+
+// Handle GameInstance detel on POST
+exports.gameinstance_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: GameInstance delete POST");
+});
+
+// Display GameInstance update form on GET
+exports.gameinstance_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: GameInstance update GET");
+});
+
+// Handle GameInstance update on POST
+exports.gameinstance_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: GameInstance update POST");
+});
