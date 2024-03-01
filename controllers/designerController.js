c7bbaabd8a8460f9933810412a9dca965f61182e
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-03-01 14:56:21 -0800

    Designer controller skeleton

---
+++ controllers/designerController.js
@@ -0,0 +1,42 @@
+const Designer = require("../models/designer");
+const asyncHandler = require("express-async-handler");
+
+// Display list of all designers
+exports.designer_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Designer List");
+});
+
+// Display detail page for a specific designer
+exports.designer_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Designer Detail - ${req.params.id}`);
+});
+
+// Display Designer create form on GET
+exports.designer_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Designer create GET");
+});
+
+// Handle Designer create on POST
+exports.designer_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Designer create POST");
+});
+
+// Display Designer delete form on GET
+exports.desiger_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Designer delete GET");
+});
+
+// Display Designer delete form on POST
+exports.designer_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Designer delete POST");
+});
+
+// Display Designer update form on GET
+exports.designer_update_get = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Designer ${req.params.id} update GET`);
+});
+
+// Display Designer update form on POST
+exports.designer_updated_post = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: Designer ${req.params.id} update POST`);
+});
