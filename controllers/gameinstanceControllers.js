9d23b11cb79c1c840371e25775f3572e3dd6296a
Alex Singh <115970252+singhalex@users.noreply.github.com>
2024-03-08 16:12:43 -0800

    Create game instance form

--- controllers/gameinstanceControllers.js
+++ controllers/gameinstanceControllers.js
@@ -1,5 +1,7 @@
 const GameInstance = require("../models/gameinstance");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+const Game = require("../models/game");
 
 // Display list of all GameInstances
 exports.gameinstance_list = asyncHandler(async (req, res, next) => {
@@ -34,13 +36,74 @@ exports.gameinstance_detail = asyncHandler(async (req, res, next) => {
 
 // Display GameInstance create form on GET
 exports.gameinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: GameInstance create GET");
+  const allGames = await Game.find({}, "title").sort({ title: 1 }).exec();
+
+  res.render("gameinstance_form", {
+    title: "Create Game Instance",
+    game_list: allGames,
+  });
 });
 
 // Handle BookInstance create on POST
-exports.gameinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: GameInstance create POST");
-});
+exports.gameinstance_create_post = [
+  // Validate and sanitize fields
+  body("game", "Game must be specified").trim().isLength({ min: 1 }).escape(),
+
+  body("price")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Price must be specified.")
+    .isCurrency({
+      require_symbol: false,
+      allow_negatives: false,
+      decimal_separator: ".",
+      require_decimal: true,
+      digits_after_decimal: [2],
+    })
+    .withMessage("Price must be a positive numeber in this format: 0.00"),
+  body("publisher", "Publisher must be specified.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status", "Status must be specified.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request after validation and sanitization
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from request
+    const errors = validationResult(req);
+
+    // Create GameInstance object with the escaped and trimmed data
+    const gameInstance = new GameInstance({
+      game: req.body.game,
+      publisher: req.body.publisher,
+      price: req.body.price,
+      status: req.body.status,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors
+      // Render form again with sanitized values and error msgs
+      const allGames = await Game.find({}, "title").sort({ title: 1 }).exec();
+
+      res.render("gameinstance_form", {
+        title: "Create Game Instance",
+        game_list: allGames,
+        selected_game: gameInstance.game._id,
+        errors: errors.array(),
+        gameinstance: gameInstance,
+      });
+      return;
+    } else {
+      // Data from form is valid
+      await gameInstance.save();
+      res.redirect(gameInstance.url);
+    }
+  }),
+];
 
 // Display GameInstance delete form on GET
 exports.gameinstance_delete_get = asyncHandler(async (req, res, next) => {
